6406e78f09b6d8ba9e9f6d6a29606e460461ecf8
András Ecker <andras.ecker@epfl.ch>
2018-06-15 17:25:36 +0200

    multiobj. optimization

--- scripts/optimization/sim_evaluator.py
+++ scripts/optimization/sim_evaluator.py
@@ -17,19 +17,6 @@ sys.path.insert(0, os.path.sep.join([SWBasePath, 'scripts']))
 from detect_oscillations import *
 
 
-def evaluate_power(ripple, gamma):
-    """helper function to score ripple/gamma power ratio"""
-    tmp = ripple/gamma
-    scoreRG = 0
-    if tmp > 20.:
-        scoreRG = 3
-    elif 10. < tmp <= 20.:
-        scoreRG = 2
-    elif 2. <= tmp <= 10.:
-        scoreRG = 1
-    return scoreRG
-
-
 class Brian2Evaluator(bpop.evaluators.Evaluator):
     """class required by BluePyOpt"""
 
@@ -44,7 +31,7 @@ class Brian2Evaluator(bpop.evaluators.Evaluator):
         # Parameters to be optimized
         self.params = [bpop.parameters.Parameter(name, bounds=(minval, maxval))
                        for name, minval, maxval in self.params]
-        self.objectives = ["fitness_score"]  # random name for BluePyOpt
+        self.objectives = ["fitness_scores"]  # random name for BluePyOpt
                        
     def generate_model(self, individual, verbose=False):
         """runs simulation (`run_sim.py`) and returns monitors"""
@@ -53,15 +40,18 @@ class Brian2Evaluator(bpop.evaluators.Evaluator):
 
     def evaluate_with_lists(self, individual):
         """fitness error used by BluePyOpt for the optimization"""
+        
         sme, smi, popre, popri = self.generate_model(individual)
         gc.collect()
         
-        fitness = 0
+        wc_errors = [0., 0., 0., 0., 0.,]  # worst case scenario
         if sme.num_spikes > 0 and smi.num_spikes > 0:  # check if there is any activity
             
             # analyse spikes
             spikeTimesE, spikingNeuronsE, poprE, ISIhist, bin_edges = preprocess_monitors(sme, popre)
             spikeTimesI, spikingNeuronsI, poprI = preprocess_monitors(smi, popri, calc_ISI=False)
+            del sme; del smi; del popre; del popri; gc.collect()
+            
             # detect replay
             avgReplayInterval = replay(ISIhist[3:16])  # bins from 150 to 850 (range of interest)
             
@@ -76,23 +66,22 @@ class Brian2Evaluator(bpop.evaluators.Evaluator):
                 avgGammaFI, gammaPI = gamma(fI, PxxI)
             
                 # look for significant ripple peak close to 180 Hz
-                ripple_peakE = 0
-                if not np.isnan(avgRippleFE):
-                    ripple_peakE = 5 - (np.abs(avgRippleFE - 180) / 180)
-                ripple_peakI = 0
-                if not np.isnan(avgRippleFI):
-                    ripple_peakI = 6 - (np.abs(avgRippleFI - 180) / 180)
-                # penalize significant gamma peak
-                bool_gammaE = int(np.isnan(avgGammaFE))
-                bool_gammaI = int(np.isnan(avgGammaFI))
-                # look for high ripple/gamma power ratio
-                ripple_gammaE = evaluate_power(ripplePE, gammaPE)
-                ripple_gammaI = evaluate_power(ripplePI, gammaPI)
-                # look for "low" population rate (gauss around 2.5Hz for exc. pop.)
-                rateE = np.exp(-1/2*(meanEr-2.5)**2/1.5**2)  # peak normalized to 1
+                ripple_peakE = np.exp(-1/2*(avgRippleFE-180.)**2/20**2)
+                ripple_peakI = 2*np.exp(-1/2*(avgRippleFI-180.)**2/20**2)
                 
-                fitness = -1 * (ripple_peakE + ripple_peakI + bool_gammaE + 1.5*bool_gammaI + ripple_gammaE + ripple_gammaI + 3*rateE)
+                # look for high ripple/gamma power ratio
+                ripple_gammaE = ripplePE/gammaPE
+                ripple_gammaI = 2*ripplePI/gammaPI
+                
+                # look for "low" exc. population rate (around 2.5 Hz)
+                rateE = np.exp(-1/2*(meanEr-2.5)**2/1.5**2)
+                
+                errors = -1 * np.linalg.norm(np.asarray(wc_errors) - np.array([ripple_peakE, ripple_peakI, ripple_gammaE, ripple_gammaI, rateE]))
         
-        return [fitness]  # single score but has to be a list for BluePyOpt
+                return errors.tolist()
+            else:
+                return wc_errors
+        else:
+            return wc_errors
     
 
