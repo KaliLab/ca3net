4e8727b8c81a580760f252fd4c7e1c62971b7ac5
András Ecker <andras.ecker@epfl.ch>
2018-09-17 10:56:28 +0200

    linear case update

--- scripts/spw_network_wmx_mult.py
+++ scripts/spw_network_wmx_mult.py
@@ -1,11 +1,10 @@
 # -*- coding: utf8 -*-
 """
 Looped version of `spw_network.py` -> checks the dynamics for different multipliers of the learned weight matrix
-authors: Bence Bagi, András Ecker last update: 08.2018
+author: András Ecker last update: 09.2018
 """
 
-import os
-import sys
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 from plots import *
@@ -28,48 +27,42 @@ if __name__ == "__main__":
     assert STDP_mode in ["sym", "asym"]
     
     place_cell_ratio = 0.5
-    detailed = True; TFR = False; analyse_LFP = False
-    que = False; save_spikes = False; verbose = False
+    linear = True
     
+    f_in = "wmx_%s_%.1f_linear.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.8, 1.3, 0.1)
+    #f_in = "wmx_%s_%.1f_2envs_linear.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.6, 1.1, 0.1)
+    #f_in = "wmx_%s_%.1f_shuffled_linear.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.8, 1.3, 0.1)
+    #f_in = "wmx_%s_%.1f_binary_linear.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(1.3, 1.8, 0.1)
+    #f_in = "wmx_%s_%.1f_block_shuffled_linear.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.8, 1.3, 0.1)    
+    print f_in
+    PF_pklf_name = os.path.join(base_path, "files", "PFstarts_%s_linear.pkl"%place_cell_ratio) if linear else None
+    f_out = "%s.txt"%f_in[4:-4]
+
+    que = False; save_spikes = False; verbose = False
+    detailed = True; TFR = False; analyse_LFP = False
     if not detailed:
         analyse_LFP = False
-        print "Without `detailed` recording LFP can't be estimated..."
     
-    f_in = "wmx_%s_%.1f.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.7, 1.4, 0.1)
-    #f_in = "wmx_%s_%.1f_shuffled.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.7, 1.4, 0.1)
-    #f_in = "wmx_%s_%.1f_binary.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(1.3, 2.0, 0.1) if STDP_mode == "sym" else np.arange(1.8, 2.5, 0.1)
-    #f_in = "wmx_%s_%.1f_block_shuffled.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.7, 1.4, 0.1)
-    #f_in = "wmx_%s_%.1f_shuffled_subpop_input.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.7, 1.4, 0.1)
-    #f_in = "wmx_%s_%.1f_2envs.pkl"%(STDP_mode, place_cell_ratio); multipliers = np.arange(0.6, 1.3, 0.1)
-    
-    
-    f_out = "%s.txt"%f_in[4:-4]
-       
     pklf_name = os.path.join(base_path, "files", f_in)
     wmx_PC_E = load_wmx(pklf_name) * 1e9  # *1e9 nS conversion
-    
-    print f_in
+        
     results = np.zeros((len(multipliers), 16))
     for i, multiplier in enumerate(multipliers):
         print "multiplier: %.2f"%multiplier
+        dir_name = os.path.join(base_path, "figures", "%.2f_replay_det_%s_%.1f"%(multiplier, STDP_mode, place_cell_ratio)) if linear else None
         
-        if detailed:
-            SM_PC, SM_BC, RM_PC, RM_BC, selection, StateM_PC, StateM_BC = run_simulation(wmx_PC_E*multiplier, STDP_mode, detailed=True,
-                                                                                         LFP=analyse_LFP, que=que, save_spikes=save_spikes, verbose=verbose)
-            results[i, :] = analyse_results(SM_PC, SM_BC, RM_PC, RM_BC, multiplier=multiplier,
-                                            detailed=True, selection=selection, StateM_PC=StateM_PC, StateM_BC=StateM_BC,
-                                            TFR=TFR, analyse_LFP=analyse_LFP, verbose=verbose)
-        else:
-            SM_PC, SM_BC, RM_PC, RM_BC = run_simulation(wmx_PC_E*multiplier, STDP_mode, detailed=False, que=que, save_spikes=save_spikes, verbose=verbose)
-            results[i, :] = analyse_results(SM_PC, SM_BC, RM_PC, RM_BC, multiplier=multiplier, detailed=False, TFR=TFR, verbose=verbose)
-        
+        SM_PC, SM_BC, RM_PC, RM_BC, selection, StateM_PC, StateM_BC = run_simulation(wmx_PC_E*multiplier, STDP_mode, detailed=detailed,
+                                                                                     LFP=analyse_LFP, que=que, save_spikes=save_spikes, verbose=verbose)
+        results[i, :] = analyse_results(SM_PC, SM_BC, RM_PC, RM_BC, multiplier=multiplier, linear=linear, pklf_name=PF_pklf_name, dir_name=dir_name,
+                                        detailed=detailed, selection=selection, StateM_PC=StateM_PC, StateM_BC=StateM_BC,
+                                        TFR=TFR, analyse_LFP=analyse_LFP, verbose=verbose)
         plt.close("all")
     
-    
+        
     f_name = os.path.join(base_path, "files", "results", f_out)
     np.savetxt(f_name, results, fmt="%.3f", delimiter="\t", header=header)
-    
-    plot_summary_replay(multipliers, avg_replay_intervals=results[:, 1], rates_PC=results[:, 2], rates_BC=results[:, 3])
+        
+    plot_summary_replay(multipliers, replay=results[:, 1], rates_PC=results[:, 2], rates_BC=results[:, 3])
     plot_summary_ripple(multipliers, ripple_freqs_PC=results[:, 4], ripple_freqs_BC=results[:, 6],
                         ripple_powers_PC=results[:, 5], ripple_powers_BC=results[:, 7])
     plot_summary_gamma(multipliers, gamma_freqs_PC=results[:, 8], gamma_freqs_BC=results[:, 10],
