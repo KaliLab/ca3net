711fc817bc8799ff15bf38f0c133ca03edbe9dcf
Andras Ecker <ecker.andris@gmail.com>
2019-02-20 17:11:55 +0100

    minor changes

--- scripts/optimization/analyse_EPS.py
+++ scripts/optimization/analyse_EPS.py
@@ -13,13 +13,13 @@ import matplotlib.pyplot as plt
 base_path = os.path.sep.join(os.path.abspath("__file__").split(os.path.sep)[:-3])
 # add "scripts" directory to the path (to import modules)
 sys.path.insert(0, os.path.sep.join([base_path, "scripts"]))
-from spw_network import load_wmx
+from helper import load_wmx
 from plots import plot_avg_EPS, plot_EPS_dist
 
 
 # synaptic parameters:
 rise_PC_E = 1.3 * ms  # Guzman 2016 (only from Fig.1 H - 20-80%)
-decay_PC_E = 9.5 * ms  # Guzman 2016 ("needed for temporal summation of EPSPs") 
+decay_PC_E = 9.5 * ms  # Guzman 2016 ("needed for temporal summation of EPSPs")
 tp = (decay_PC_E * rise_PC_E)/(decay_PC_E - rise_PC_E) * np.log(decay_PC_E/rise_PC_E)  # time to peak
 norm_PC_E = 1.0 / (np.exp(-tp/decay_PC_E) - np.exp(-tp/rise_PC_E))
 delay_PC_E = 2.2 * ms  # Guzman 2016
@@ -41,11 +41,11 @@ b_PC = 0.22030375858 * nA
 tau_w_PC = 80.1747780694 * ms
 
 eqs_PC = """
-dvm/dt = (-g_leak_PC*(vm-Vrest_PC) + g_leak_PC*delta_T_PC*exp((vm- theta_PC)/delta_T_PC) - w + I - EPSC)/Cm_PC : volt (unless refractory)
+dvm/dt = (-g_leak_PC*(vm-Vrest_PC) + g_leak_PC*delta_T_PC*exp((vm- theta_PC)/delta_T_PC) - w + I + EPSC)/Cm_PC : volt (unless refractory)
 dw/dt = (a_PC*(vm- Vrest_PC )-w)/tau_w_PC : amp
 dg_ampa/dt = (x_ampa - g_ampa) / rise_PC_E : 1
 dx_ampa/dt = -x_ampa / decay_PC_E : 1
-EPSC = g_ampa*z*(vm-Erev_E): amp
+EPSC = -g_ampa*z*(vm-Erev_E): amp
 I : amp
 """
 
@@ -57,28 +57,28 @@ def sym_paired_recording(weight, i_hold=None):
     :param i_hold: holding current applied to clamp the postsynaptic cell
     :return: time, EPSP and EPSC
     """
-    
+
     np.random.seed(12345)
     pyrandom.seed(12345)
-    
+
     # postsynaptic neuron
     PC = NeuronGroup(1, model=eqs_PC, threshold="vm>spike_th_PC",
                      reset="vm=Vreset_PC; w+=b_PC", refractory=tref_PC, method="exponential_euler")
     PC.vm = Vrest_PC; PC.g_ampa = 0.0
-    
+
     # presynaptic neuron is modelled only as a spike generator
     pre = SpikeGeneratorGroup(1, np.array([0]), np.array([250])*ms)
-    
-    C_PC_E = Synapses(pre, PC, on_pre="x_ampa+=norm_PC_E*weight", delay=delay_PC_E)    
+
+    C_PC_E = Synapses(pre, PC, on_pre="x_ampa+=norm_PC_E*weight", delay=delay_PC_E)
     C_PC_E.connect(i=0, j=0)
-    
+
     StateM_PC = StateMonitor(PC, variables=["vm", "EPSC"], record=True, dt=0.1*ms)
-    
+
     run(10*ms)
     if i_hold:
         PC.I = i_hold * pA  # holding current (has to be precalculated)
     run(390*ms)
-    
+
     return StateM_PC.t_ * 1000, StateM_PC[0].vm/mV, StateM_PC[0].EPSC/pA  # t, EPSP, EPSC
 
 
@@ -89,8 +89,8 @@ def get_peak_EPSP(t_, EPSP, i_hold=None, v_hold=None):
     :param i_hold: holding current
     :param v_hold: holding voltage
     :return: peak EPSP
-    """   
-    
+    """
+
     if i_hold:
         assert v_hold, "if I_hold is specified, V_hold has to be specified too"
         return np.max(EPSP[np.where((250 < t_) & (t_ < 350))]) - v_hold
@@ -102,44 +102,36 @@ def get_peak_EPSP(t_, EPSP, i_hold=None, v_hold=None):
 if __name__ == "__main__":
 
     try:
-        n = int(sys.argv[1])       
+        n = int(sys.argv[1])
     except:
         n = 500  # number of random weights
-    
+
     v_hold = -70.  # mV
     i_hold = -43.638  # pA (calculated by `clamp_cell.py`)
-    
-    STDP_mode = "asym"
-    place_cell_ratio = 0.5
-    f_in = "wmx_%s_%.1f.pkl"%(STDP_mode, place_cell_ratio)
-    
-    pklf_name = os.path.join(base_path, "files", f_in)
+
+    f_in = "wmx_sym_0.5_linear.pkl"
+    pklf_name = os.path.join(base_path, "files", "paper", f_in)
     wmx_PC_E = load_wmx(pklf_name) * 1e9  # *1e9 nS conversion
 
     nonzero_weights = wmx_PC_E[np.nonzero(wmx_PC_E)]
     print "mean(nonzero weights): %s (nS)"%np.mean(nonzero_weights)
-    
+
+    np.random.seed(12345)
     weights = np.random.choice(nonzero_weights, n, replace=False)
-    
-    EPSPs = np.zeros((n, 4000)); EPSCs = np.zeros((n, 4000))  # 4000 is hard coded for sim length    
+
+    EPSPs = np.zeros((n, 4000)); EPSCs = np.zeros((n, 4000))  # 4000 is hard coded for sim length
     peak_EPSPs = np.zeros(n); peak_EPSCs = np.zeros(n)
-    for i, weight in enumerate(weights):   
-     
+    for i, weight in enumerate(weights):
         t_, EPSP, EPSC = sym_paired_recording(weight, i_hold)
-        
         EPSPs[i,:] = EPSP; EPSCs[i,:] = EPSC
         peak_EPSPs[i] = get_peak_EPSP(t_, EPSP, i_hold, v_hold)
-        peak_EPSCs[i] = np.min(EPSC)
-        
+        peak_EPSCs[i] = np.max(EPSC)
         if i % 50 == 0:
             print("%s/%s done..."%(i, n))
 
-    # finall run with the average of all nonzero weights
     t_, EPSP, EPSC = sym_paired_recording(np.mean(nonzero_weights), i_hold)
 
-    # Plots
-    plot_avg_EPS(t_, EPSPs, EPSP, EPSCs, EPSC, np.mean(nonzero_weights), "EPS*_%s"%STDP_mode)
-    plot_EPS_dist(peak_EPSPs, peak_EPSCs, "distEPS*_%s"%STDP_mode)
+
+    plot_avg_EPS(t_, EPSPs, EPSP, EPSCs, EPSC, np.mean(nonzero_weights), "EPS*_sym")
+    plot_EPS_dist(peak_EPSPs, peak_EPSCs, "distEPS*_sym")
     plt.show()
-    
-    
--- scripts/optimization/clamp_cell.py
+++ scripts/optimization/clamp_cell.py
@@ -46,32 +46,32 @@ def _calculate_SS_voltage(i, check_for_spiking=True, plot_=False):
     :param plot_: bool for plotting voltages
     :return: SS_voltage: steady stage voltage for a given input current
     """
-    
+
     np.random.seed(12345)
     pyrandom.seed(12345)
-    
+
     PC = NeuronGroup(1, model=eqs_PC, threshold="vm>spike_th_PC",
                      reset="vm=Vreset_PC; w+=b_PC", refractory=tref_PC, method="exponential_euler")
     PC.vm = Vrest_PC
-    
+
     StateM_PC = StateMonitor(PC, "vm", record=True, dt=0.1*ms)
-    
+
     run(200 * ms)
     PC.I = i * pA
     run(800 * ms)
-    
+
     t = StateM_PC.t_ * 1000  # *1000 ms convertion
     v = StateM_PC[0].vm/mV
-        
+
     SS_voltage = np.mean(v[np.where((800. < t) & (t < 1000.))])
-    
+
     if plot_:
         plot_SS_voltage(t, v, SS_voltage, i)
-        
+
     if check_for_spiking:
         if len(np.nonzero(v[np.where(100.0 < t)] > 0.)[0]) > 0:  # check for voltage crossings
             return None
-    
+
     return SS_voltage
 
 
@@ -82,29 +82,29 @@ def holding_current(currents, v_hold):
     :param v_hold: target voltage
     :return: i_hold: current necessary to clamp the cell at the given voltage
     """
-    
+
     # get SS voltages for different holding currents
     voltages = []
     for i in currents:
         voltages.append(_calculate_SS_voltage(i, check_for_spiking=False))
-        
+
     # get an estimate of the root of IV curve
     (m, bis) = np.polyfit(currents, voltages, 1)  # fit a linear line (IV curve)
     center = (v_hold - bis) / m  # center: estimated clamping current (to hold the cell at v_hold)
-    
+
     def iv(i):
         """
         Local helper function for finding the bisection point of the IV curve
         :param i: holding current (see `_calculate_SS_voltage()`)
         :return: diff between steady stage voltage (for a given current) and desired holding current
         """
-        
+
         SS_voltage = _calculate_SS_voltage(i)
         if SS_voltage is not None:
             return SS_voltage - v_hold
         else:
             return 20.  # return large diff. above v_hold and let the program run instead of throwing an error
-            
+
     # zoom in a bit
     failed = False
     dx = 1.5
@@ -128,24 +128,18 @@ def holding_current(currents, v_hold):
 
     # get more precise bisection point
     i_hold = bisect(iv, a, b, xtol=0.01, maxiter=100)
-    
-    # plot for i_hold
+
     _calculate_SS_voltage(i_hold, check_for_spiking=True, plot_=True)
 
     return i_hold
- 
+
 if __name__ == "__main__":
-    
+
     try:
-        v_hold = float(sys.argv[1])       
+        v_hold = float(sys.argv[1])
     except:
         v_hold = -70.  # mV
     currents = [-55, -50, -45]  # pA
-    
+
     i_hold = holding_current(currents, v_hold)
     print("Holding current for %s mV is %.3f pA"%(v_hold, i_hold))
-    
-    
-    
-    
-    
