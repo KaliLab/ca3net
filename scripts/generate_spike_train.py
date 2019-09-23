5509d449956ab8286b23b10b8bffa87b2075b07f
Andras Ecker <ecker.andris@gmail.com>
2019-09-23 17:06:45 +0200

    minor fixes

--- scripts/generate_spike_train.py
+++ scripts/generate_spike_train.py
@@ -29,24 +29,27 @@ def generate_spike_train(n_neurons, place_cell_ratio, linear, ordered=True, seed
     :return: spike_trains - list of lists with indiviual neuron's spikes
     """
 
-    assert n_neurons >= 1000, "The assumptions made during the setup hold only for a reasonably big group of neurons" 
+    assert n_neurons >= 1000, "The assumptions made during the setup hold only for a reasonably big group of neurons"
 
     neuronIDs = np.arange(0, n_neurons)
     # generate random neuronIDs being place cells and starting points for place fields
     if ordered:
         np.random.seed(seed)
 
-        p_uniform = 1./n_neurons
-        tmp = (1 - 2*2*100*p_uniform)/(n_neurons-200)
-        p = np.concatenate([2*p_uniform*np.ones(100), tmp*np.ones(n_neurons-2*100), 2*p_uniform*np.ones(100)])  # slightly oversample (double prop.) the 2 ends (first and last 100 neurons) of the track
-        place_cells = np.sort(np.random.choice(neuronIDs, int(n_neurons*place_cell_ratio), p=p, replace=False), kind="mergsort")
+        if linear:
+            p_uniform = 1./n_neurons
+            tmp = (1 - 2*2*100*p_uniform)/(n_neurons-200)
+            p = np.concatenate([2*p_uniform*np.ones(100), tmp*np.ones(n_neurons-2*100), 2*p_uniform*np.ones(100)])  # slightly oversample (double prop.) the 2 ends (first and last 100 neurons) of the track
+            place_cells = np.sort(np.random.choice(neuronIDs, int(n_neurons*place_cell_ratio), p=p, replace=False), kind="mergsort")
+        else:
+            place_cells = np.sort(np.random.choice(neuronIDs, int(n_neurons*place_cell_ratio), replace=False), kind="mergsort")
         phi_starts = np.sort(np.random.rand(n_neurons), kind="mergesort")[place_cells] * 2*np.pi
 
-        if not linear:
-            pklf_name = os.path.join(base_path, "files", "PFstarts_%s.pkl"%place_cell_ratio)
-        else:
+        if linear:
             phi_starts -= 0.1*np.pi  # shift half a PF against boundary effects (mid_PFs will be in [0, 2*np.pi]...)
             pklf_name = os.path.join(base_path, "files", "PFstarts_%s_linear.pkl"%place_cell_ratio)
+        else:
+            pklf_name = os.path.join(base_path, "files", "PFstarts_%s.pkl"%place_cell_ratio)
 
     else:
         np.random.seed(seed+1)
@@ -54,11 +57,12 @@ def generate_spike_train(n_neurons, place_cell_ratio, linear, ordered=True, seed
         place_cells = np.random.choice(neuronIDs, int(n_neurons*place_cell_ratio), replace=False)
         phi_starts = np.sort(np.random.rand(n_neurons)[place_cells], kind="mergesort") * 2*np.pi
 
-        if not linear:
-            pklf_name = os.path.join(base_path, "files", "PFstarts_%s_no.pkl"%place_cell_ratio)
-        else:
+        if linear:
             phi_starts -= 0.1*np.pi  # shift half a PF against boundary effects (mid_PFs will be in [0, 2*np.pi]...)
             pklf_name = os.path.join(base_path, "files", "PFstarts_%s_linear_no.pkl"%place_cell_ratio)
+        else:
+            pklf_name = os.path.join(base_path, "files", "PFstarts_%s_no.pkl"%place_cell_ratio)
+
 
     place_fields = {neuron_id:phi_starts[i] for i, neuron_id in enumerate(place_cells)}
     save_place_fields(place_fields, pklf_name)
