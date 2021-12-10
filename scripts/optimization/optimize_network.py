035d8162c397a875d405fba67492ba61d3141896
Andras Ecker <ecker.andris@gmail.com>
2021-12-10 16:53:47 +0100

    optimization scripts updated

--- scripts/optimization/optimize_network.py
+++ scripts/optimization/optimize_network.py
@@ -4,16 +4,19 @@ Optimizes connection parameters (synaptic weights)
 authors: Bence Bagi, András Ecker last update: 10.2018
 """
 
-import os, sys, pickle, logging
+import os, sys, logging
 import numpy as np
+import pandas as pd
 import bluepyopt as bpop
 import multiprocessing as mp
+import sim_evaluator
 base_path = os.path.sep.join(os.path.abspath("__file__").split(os.path.sep)[:-3])
 # add "scripts" directory to the path (to import modules)
 sys.path.insert(0, os.path.sep.join([base_path, "scripts"]))
-from detect_oscillations import preprocess_monitors, slice_high_activity, analyse_rate, ripple, gamma
+from helper import load_wmx, preprocess_monitors
+from detect_oscillations import analyse_rate, ripple
+from detect_replay import slice_high_activity
 from plots import plot_evolution, plot_raster, plot_PSD, plot_zoomed
-import sim_evaluator
 
 # print info into console
 logging.basicConfig(stream=sys.stdout)
@@ -21,106 +24,117 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def load_wmx(pklf_name):
+def load_checkpoints(pklf_name):
     """
-    Dummy function to load in the excitatory weight matrix and make python clear the memory
-    :param pklf_name: file name of the saved weight matrix
-    :return: wmx_PC_E: excitatory weight matrix
-    """
-
+    Loads in saved checkpoints from pickle file (used e.g. to repeat analysis...)
+    :param pklf_name: name of the saved pickle file
+    :return: obejects saved by BluePyOpt"""
+    import pickle
     with open(pklf_name, "rb") as f:
-        wmx_PC_E = pickle.load(f)
+        cp = pickle.load(f)
+    return cp["generation"], cp["halloffame"], cp["logbook"], cp["history"]
 
-    return wmx_PC_E
+
+def hof2csv(pnames, hof, f_name):
+    """
+    Creates pandas DaataFrame from hall of fame and saves it to csv
+    :param pnames: names of optimized parameters
+    :param hof: BluePyOpt HallOfFame object
+    :param f_name: name of the saved file
+    """
+    data = np.zeros((len(hof), len(pnames)))
+    for i in range(len(hof)):
+        data[i, :] = hof[i]
+    df = pd.DataFrame(data=data, columns=pnames)
+    df.to_csv(f_name)
 
 
-def analyse_results(SM_PC, SM_BC, RM_PC, RM_BC):
+def analyse_results(SM_PC, SM_BC, RM_PC, RM_BC, linear):
     """
     Duplicated (simpler) version of `../spw_network.py/analyse_results()`
     :param SM_PC, SM_BC, RM_PC, RM_BC: Brian2 spike and rate monitors of PC and BC populations
     """
 
     if SM_PC.num_spikes > 0 and SM_BC.num_spikes > 0:  # check if there is any activity
-
         spike_times_PC, spiking_neurons_PC, rate_PC, ISI_hist_PC, bin_edges_PC = preprocess_monitors(SM_PC, RM_PC)
         spike_times_BC, spiking_neurons_BC, rate_BC = preprocess_monitors(SM_BC, RM_BC, calc_ISI=False)
 
-        slice_idx = slice_high_activity(rate_PC)
-        mean_rate_PC, rate_ac_PC, max_ac_PC, t_max_ac_PC, f_PC, Pxx_PC = analyse_rate(rate_PC, 1000.0, slice_idx, TFR=False)
-        mean_rate_BC, rate_ac_BC, max_ac_BC, t_max_ac_BC, f_BC, Pxx_BC = analyse_rate(rate_BC, 1000.0, slice_idx, TFR=False)
+        slice_idx = [] if not linear else slice_high_activity(rate_PC, th=2, min_len=260)
+        mean_rate_PC, rate_ac_PC, max_ac_PC, t_max_ac_PC, f_PC, Pxx_PC = analyse_rate(rate_PC, 1000., slice_idx)
+        mean_rate_BC, rate_ac_BC, max_ac_BC, t_max_ac_BC, f_BC, Pxx_BC = analyse_rate(rate_BC, 1000., slice_idx)
 
-        max_ac_ripple_PC, t_max_ac_ripple_PC, avg_ripple_freq_PC, ripple_power_PC = ripple(rate_ac_PC, f_PC, Pxx_PC, slice_idx)
-        max_ac_ripple_BC, t_max_ac_ripple_BC, avg_ripple_freq_BC, ripple_power_BC = ripple(rate_ac_BC, f_BC, Pxx_BC, slice_idx)
-        avg_gamma_freq_PC, gamma_power_PC = gamma(f_PC, Pxx_PC, slice_idx)
-        avg_gamma_freq_BC, gamma_power_BC = gamma(f_BC, Pxx_BC, slice_idx)
+        avg_ripple_freq_PC, ripple_power_PC = ripple(f_PC, Pxx_PC, slice_idx)
+        avg_ripple_freq_BC, ripple_power_BC = ripple(f_BC, Pxx_BC, slice_idx)
 
-        print "Mean excitatory rate: %.3f"%mean_rate_PC
-        print "Mean inhibitory rate: %.3f"%mean_rate_BC
-        print "Average exc. ripple freq: %.3f"%avg_ripple_freq_PC
-        print "Exc. ripple power: %.3f"%ripple_power_PC
-        print "Average inh. ripple freq: %.3f"%avg_ripple_freq_BC
-        print "Inh. ripple power: %.3f"%ripple_power_BC
+        print("Mean excitatory rate: %.3f" % mean_rate_PC)
+        print("Mean inhibitory rate: %.3f" % mean_rate_BC)
+        print("Average exc. ripple freq: %.3f" % avg_ripple_freq_PC)
+        print("Exc. ripple power: %.3f" % ripple_power_PC)
+        print("Average inh. ripple freq: %.3f" % avg_ripple_freq_BC)
+        print("Inh. ripple power: %.3f" % ripple_power_BC)
 
-        plot_raster(spike_times_PC, spiking_neurons_PC, rate_PC, [ISI_hist_PC, bin_edges_PC], True, "blue", multiplier_=1)
+        plot_raster(spike_times_PC, spiking_neurons_PC, rate_PC, [ISI_hist_PC, bin_edges_PC], slice_idx, "blue", multiplier_=1)
         plot_PSD(rate_PC, rate_ac_PC, f_PC, Pxx_PC, "PC_population", "blue", multiplier_=1)
         plot_PSD(rate_BC, rate_ac_BC, f_BC, Pxx_BC, "BC_population", "green", multiplier_=1)
-        plot_zoomed(spike_times_PC, spiking_neurons_PC, rate_PC, "PC_population", "blue", multiplier_=1)
+        plot_zoomed(spike_times_PC, spiking_neurons_PC, rate_PC, "PC_population", "blue", multiplier_=1, PC_pop=False)
         plot_zoomed(spike_times_BC, spiking_neurons_BC, rate_BC, "BC_population", "green", multiplier_=1, PC_pop=False)
 
     else:
-        print "No activity !"
+        print("No activity!")
 
 
 if __name__ == "__main__":
-
     try:
         STDP_mode = sys.argv[1]
     except:
-        STDP_mode = "asym"
+        STDP_mode = "sym"
     assert STDP_mode in ["sym", "asym"]
+    linear = False
     place_cell_ratio = 0.5
-    f_in = "wmx_%s_%.1f_linear.pkl"%(STDP_mode, place_cell_ratio)
+    f_in = "wmx_%s_%.1f_linear.pkl" % (STDP_mode, place_cell_ratio) if linear else "wmx_%s_%.1f.pkl" % (STDP_mode, place_cell_ratio)
+    cp_f_name = os.path.join(base_path, "scripts", "optimization", "checkpoints", "checkpoint_%s" % f_in[4:])
+    hof_f_name = os.path.join(base_path, "scripts", "optimization", "checkpoints", "hof_%s" % f_in[4:])
 
     # parameters to be fitted as a list of: (name, lower bound, upper bound)
+    # the order matters! if you want to add more parameters - update `run_sim.py` too
     optconf = [("w_PC_I_", 0.15, 0.4),
                ("w_BC_E_", 3.25, 4.25),
                ("w_BC_I_", 6.5, 8.0),
                ("wmx_mult_", 1.0, 3.0),
                ("w_PC_MF_", 24.0, 26.0),
                ("rate_MF_", 15.0, 17.0)]
-               # the order matters! if you want to add more parameters - update `run_sim.py` too
     pnames = [name for name, _, _ in optconf]
 
-    offspring_size = 35
-    max_ngen = 10
+    offspring_size = 3
+    max_ngen = 2
 
     pklf_name = os.path.join(base_path, "files", f_in)
     wmx_PC_E = load_wmx(pklf_name) * 1e9  # *1e9 nS conversion
 
     # Create multiprocessing pool for parallel evaluation of fitness function
-    pool = mp.Pool(processes=mp.cpu_count())
-
+    pool = mp.Pool(processes=mp.cpu_count()-1)
     # Create BluePyOpt optimization and run
-    evaluator = sim_evaluator.Brian2Evaluator(wmx_PC_E, optconf)
+    evaluator = sim_evaluator.Brian2Evaluator(linear, wmx_PC_E, optconf)
+    '''
     opt = bpop.optimisations.DEAPOptimisation(evaluator, offspring_size=offspring_size, map_function=pool.map,
                                               eta=20, mutpb=0.3, cxpb=0.7)
 
-    print "Started running %i simulations on %i cores..."%(offspring_size*max_ngen ,mp.cpu_count())
-    pop, hof, log, hist = opt.run(max_ngen=max_ngen,
-                                  cp_filename=os.path.join(base_path, "scripts", "optimization", "checkpoints", "checkpoint_%.1f_%s.pkl"%(place_cell_ratio, STDP_mode)))
-    del pool; del opt
-
+    print("Started running %i simulations on %i cores..." % (offspring_size*max_ngen, mp.cpu_count()-1))
+    pop, hof, log, hist = opt.run(max_ngen=max_ngen, cp_filename=cp_f_name)
+    del pool, opt
+    '''
     # ====================================== end of optimization ======================================
+
+    pop, hof, log, hist = load_checkpoints(cp_f_name)
 
     # summary figure (about optimization)
     plot_evolution(log.select("gen"), np.array(log.select("min")), np.array(log.select("avg")),
                    np.array(log.select("std")), "fittnes_evolution")
 
-
-    best = hof[0]  # get best individual
+    # save hall of fame to csv, get best individual, and rerun with best parameters to save figures
+    hof2csv(pnames, hof, hof_f_name)
+    best = hof[0]
     for pname, value in zip(pnames, best):
-        print "%s = %.3f"% (pname, value)
-
-    # rerun with best parameters and save figures
+        print("%s = %.3f" % (pname, value))
     SM_PC, SM_BC, RM_PC, RM_BC = evaluator.generate_model(best, verbose=True)
-    analyse_results(SM_PC, SM_BC, RM_PC, RM_BC)
+    analyse_results(SM_PC, SM_BC, RM_PC, RM_BC, linear)
--- scripts/optimization/sim_evaluator.py
+++ scripts/optimization/sim_evaluator.py
@@ -1,28 +1,31 @@
 # -*- coding: utf8 -*-
 """
 BluePyOpt evaluator for optimization
-authors: Bence Bagi, András Ecker, Szabolcs Káli last update: 06.2018
+authors: Bence Bagi, András Ecker, Szabolcs Káli last update: 12.2021
 """
 
-import os, sys, gc
+import os, sys, traceback, gc
 import numpy as np
 import bluepyopt as bpop
 import run_sim as sim
 base_path = os.path.sep.join(os.path.abspath("__file__").split(os.path.sep)[:-3])
 # add "scripts" directory to the path (to import modules)
 sys.path.insert(0, os.path.sep.join([base_path, "scripts"]))
-from detect_oscillations import preprocess_monitors, slice_high_activity, analyse_rate, ripple, gamma
+from helper import preprocess_monitors
+from detect_oscillations import analyse_rate, ripple, gamma
+from detect_replay import slice_high_activity
 
 
 class Brian2Evaluator(bpop.evaluators.Evaluator):
     """Evaluator class required by BluePyOpt"""
 
-    def __init__(self, Wee, params):
+    def __init__(self, linear, Wee, params):
         """
         :param Wee: weight matrix (passing Wee with cPickle to the slaves (as BluPyOpt does) is still the fastest solution)
         :param params: list of parameters to fit - every entry must be a tuple: (name, lower bound, upper bound)
         """
         super(Brian2Evaluator, self).__init__()
+        self.linear = linear
         self.Wee = Wee
         self.params = params
 
@@ -33,51 +36,45 @@ class Brian2Evaluator(bpop.evaluators.Evaluator):
 
     def generate_model(self, individual, verbose=False):
         """Runs single simulation (see `run_sim.py`) and returns monitors"""
-
         SM_PC, SM_BC, RM_PC, RM_BC = sim.run_simulation(self.Wee, *individual, verbose=verbose)
         return SM_PC, SM_BC, RM_PC, RM_BC
 
     def evaluate_with_lists(self, individual):
         """Fitness error used by BluePyOpt for the optimization"""
-
         SM_PC, SM_BC, RM_PC, RM_BC = self.generate_model(individual)
-        gc.collect()
 
-        wc_errors = [0., 0., 0., 0., 0., 0.]  # worst case scenario
-        if SM_PC.num_spikes > 0 and SM_BC.num_spikes > 0:  # check if there is any activity
+        try:
+            wc_errors = [0., 0., 0., 0., 0., 0.]  # worst case scenario
+            if SM_PC.num_spikes > 0 and SM_BC.num_spikes > 0:  # check if there is any activity
+                # analyse spikes
+                spike_times_PC, spiking_neurons_PC, rate_PC, ISI_hist_PC, bin_edges_PC = preprocess_monitors(SM_PC, RM_PC)
+                spike_times_BC, spiking_neurons_BC, rate_BC = preprocess_monitors(SM_BC, RM_BC, calc_ISI=False)
+                del SM_PC, SM_BC, RM_PC, RM_BC
+                gc.collect()
+                # analyse rates
+                slice_idx = [] if not self.linear else slice_high_activity(rate_PC, th=2, min_len=260)
+                mean_rate_PC, rate_ac_PC, max_ac_PC, t_max_ac_PC, f_PC, Pxx_PC = analyse_rate(rate_PC, 1000.0, slice_idx)
+                mean_rate_BC, rate_ac_BC, max_ac_BC, t_max_ac_BC, f_BC, Pxx_BC = analyse_rate(rate_BC, 1000.0, slice_idx)
+                avg_ripple_freq_PC, ripple_power_PC = ripple(f_PC, Pxx_PC, slice_idx)
+                avg_ripple_freq_BC, ripple_power_BC = ripple(f_BC, Pxx_BC, slice_idx)
+                avg_gamma_freq_PC, gamma_power_PC = gamma(f_PC, Pxx_PC, slice_idx)
+                avg_gamma_freq_BC, gamma_power_BC = gamma(f_BC, Pxx_BC, slice_idx)
 
-            # analyse spikes
-            spike_times_PC, spiking_neurons_PC, rate_PC, ISI_hist_PC, bin_edges_PC = preprocess_monitors(SM_PC, RM_PC)
-            spike_times_BC, spiking_neurons_BC, rate_BC = preprocess_monitors(SM_BC, RM_BC, calc_ISI=False)
-            del SM_PC; del SM_BC; del RM_PC; del RM_BC; gc.collect()
-
-            # analyse rates
-            slice_idx = slice_high_activity(rate_PC)
-            mean_rate_PC, rate_ac_PC, max_ac_PC, t_max_ac_PC, f_PC, Pxx_PC = analyse_rate(rate_PC, 1000.0, slice_idx, TFR=False)
-            mean_rate_BC, rate_ac_BC, max_ac_BC, t_max_ac_BC, f_BC, Pxx_BC = analyse_rate(rate_BC, 1000.0, slice_idx, TFR=False)
-
-            max_ac_ripple_PC, t_max_ac_ripple_PC, avg_ripple_freq_PC, ripple_power_PC = ripple(rate_ac_PC, f_PC, Pxx_PC, slice_idx)
-            max_ac_ripple_BC, t_max_ac_ripple_BC, avg_ripple_freq_BC, ripple_power_BC = ripple(rate_ac_BC, f_BC, Pxx_BC, slice_idx)
-            avg_gamma_freq_PC, gamma_power_PC = gamma(f_PC, Pxx_PC, slice_idx)
-            avg_gamma_freq_BC, gamma_power_BC = gamma(f_BC, Pxx_BC, slice_idx)
-
-            # look for significant ripple peak close to 180 Hz
-            ripple_peakE = np.exp(-1/2*(avg_ripple_freq_PC-180.)**2/20**2) if not np.isnan(avg_ripple_freq_PC) else 0.
-            ripple_peakI = 2*np.exp(-1/2*(avg_ripple_freq_BC-180.)**2/20**2) if not np.isnan(avg_ripple_freq_BC) else 0.
-
-            # penalize gamma peak (in inhibitory pop)
-            no_gamma_peakI = 1. if np.isnan(avg_gamma_freq_BC) else 0.  # this is a binary variable, which might not be the best for this algorithm
-
-            # look for high ripple/gamma power ratio
-            ripple_ratioE = np.clip(ripple_power_PC/gamma_power_PC, 0., 5.)
-            ripple_ratioI = np.clip(2*ripple_power_BC/gamma_power_BC, 0., 10.)
-
-            # look for "low" exc. population rate (around 2.5 Hz)
-            rateE = np.exp(-1/2*(mean_rate_PC-2.5)**2/1.0**2)
-
-            # *-1 since the algorithm tries to minimize...
-            errors = -1. * np.array([ripple_peakE, ripple_peakI, no_gamma_peakI, ripple_ratioE, ripple_ratioI, rateE])
-
-            return errors.tolist()
-        else:
-            return wc_errors
+                # look for significant ripple peak close to 180 Hz
+                ripple_peakE = np.exp(-1/2*(avg_ripple_freq_PC-180.)**2/20**2) if not np.isnan(avg_ripple_freq_PC) else 0.
+                ripple_peakI = 2*np.exp(-1/2*(avg_ripple_freq_BC-180.)**2/20**2) if not np.isnan(avg_ripple_freq_BC) else 0.
+                # penalize gamma peak (in inhibitory pop) - binary variable, which might not be the best for this algo.
+                no_gamma_peakI = 1. if np.isnan(avg_gamma_freq_BC) else 0.
+                # look for high ripple/gamma power ratio
+                ripple_ratioE = np.clip(ripple_power_PC/gamma_power_PC, 0., 5.)
+                ripple_ratioI = np.clip(2*ripple_power_BC/gamma_power_BC, 0., 10.)
+                # look for "low" exc. population rate (around 2.5 Hz)
+                rateE = np.exp(-1/2*(mean_rate_PC-2.5)**2/1.0**2)
+                # *-1 since the algorithm tries to minimize...
+                errors = -1. * np.array([ripple_peakE, ripple_peakI, no_gamma_peakI, ripple_ratioE, ripple_ratioI, rateE])
+                return errors.tolist()
+            else:
+                return wc_errors
+        except Exception:
+            # Make sure exception and backtrace are thrown back to parent process
+            raise Exception("".join(traceback.format_exception(*sys.exc_info())))
